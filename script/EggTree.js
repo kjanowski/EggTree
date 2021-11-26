64c9f5f5358f3a6214012269191fa97e58fd7a9d
Kathrin Janowski <science@kathrinjanowski.com>
2021-11-26 19:39:33 +0100

    initial commit

---
+++ script/EggTree.js
@@ -0,0 +1,24 @@
+var family=undefined;
+
+
+function loadFamily(familyURL)
+{
+	var xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			initFamily(this.responseText);
+		}
+	};
+	xmlhttp.open("GET", configURL, true);
+	xmlhttp.send();
+}
+
+
+function initFamily(jsonData)
+{
+	family = JSON.parse(jsonData);
+	
+	var output = document.getElementById("eggTree");
+	output.innerHTML = ""+family;
+}
