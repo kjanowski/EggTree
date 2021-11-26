3d01cb3f2c7e4c1be89f1cd983a28bdb7c5395ca
Kathrin Janowski <science@kathrinjanowski.com>
2021-11-27 00:00:12 +0100

    cleanup

--- script/EggTree.js
+++ script/EggTree.js
@@ -32,31 +32,45 @@ function initFamily(jsonData)
 }
 
 
-function displayPerson(id)
+function createPersonSVG(id)
 {
-	var output = document.getElementById("eggTree");
-	
-	var person = family.people[id];
-	
-	var nameText = person.name.given+" "+person.name.father+" "+person.name.birth_clan;
-	if(person.name.married_clan != undefined)
-		nameText += " "+person.name.married_clan;
-	
-	var hatchedText= "hatched "+person.hatch_date.year+"/"+person.hatch_date.month+"/"+person.hatch_date.day;
-
-	
-	var posX = person.x - drawParams.halfCardWidth;
-	var posY = person.y - drawParams.halfCardHeight;
+	var posX = person.x*1 - drawParams.halfCardWidth;
+	var posY = person.y*1 - drawParams.halfCardHeight;
 	var topBlocksY = posY + drawParams.cardPadding;
 	var leftBlockX = posX + drawParams.cardPadding;
 	var rightBlockX = posX + drawParams.cardPadding*2 + drawParams.imgSize;
 	var bottomBlockY = posY + drawParams.cardPadding;
 	var bottomBlockX = posX + drawParams.cardPadding;
+
+	var person = family.people[id];
 	
-	output.innerHTML ="<g id=\"person_"+id+"\">"
-				+"<rect class=\"card\" x=\""+posX+"\" y=\""+posY+"\" width=\"500\" height=\"300\"/>"
-				+"<image class=\"portrait\" x=\""+leftBlockX+"\" y=\""+topBlocksY+"\" width=\"200\" height=\"200\" xlink:href=\""+person.image_url+"\"/>"
-				+"<text class=\"name\" x=\""+rightBlockX+"\" y=\""+topBlocksY+"\">"+nameText+"</text>"
-				+"<text class=\"name\" x=\""+rightBlockX+"\" y=\""+(topBlocksY+drawParams.lineHeight)+"\">"+hatchedText+"</text>"
-				+"</g>";
+	//begin the group element
+	var svgGroup = "<g id=\"person_"+id+"\">"
+				+"<rect class=\"card\" x=\""+posX+"\" y=\""+posY+"\" width=\"500\" height=\"300\"/>";
+
+	//add the portrait
+	svgGroup += "<image class=\"portrait\" x=\""+leftBlockX+"\" y=\""+topBlocksY+"\" width=\"200\" height=\"200\" xlink:href=\""+person.image_url+"\"/>";
+	
+	//add the name
+	var nameText = person.name.given+" "+person.name.father+" "+person.name.birth_clan;
+	if(person.name.married_clan != undefined)
+		nameText += " "+person.name.married_clan;
+	svgGroup += "<text class=\"name\" x=\""+rightBlockX+"\" y=\""+topBlocksY+"\">"+nameText+"</text>"
+	
+	
+	//add the hatching date
+	var hatchedText= "hatched "+person.hatch_date.year+"/"+person.hatch_date.month+"/"+person.hatch_date.day;
+	svgGroup += "<text class=\"name\" x=\""+rightBlockX+"\" y=\""+(topBlocksY+drawParams.lineHeight)+"\">"+hatchedText+"</text>"
+				
+	//close the group element
+	svgGroup += "</g>";
+
+	return svgGroup;
+}	
+
+
+function displayPerson(id)
+	//assemble it all
+	var output = document.getElementById("eggTree");
+	output.innerHTML = createPersonSVG(id);
 }
